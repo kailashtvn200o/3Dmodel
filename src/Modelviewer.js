eea24e563db68e4da75eccc891e31cc4da64960a
kailashtvn200o <kailashtvn2000@gmail.com>
2023-09-05 17:58:00 +0530

    unwanted commit

--- src/Modelviewer.js
+++ src/Modelviewer.js
@@ -3,26 +3,26 @@ import { Canvas, useLoader, useFrame,extend, useThree } from 'react-three-fiber'
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader';
 import { OrbitControls,PerspectiveCamera } from '@react-three/drei';
 
-// extend({ OrbitControls });
+
 
 function ModelViewer() {
-  const gltfUrl = '/scene.gltf'; // Adjust the path to your GLTF model
+  const gltfUrl = '/scene.gltf'; 
 
-  // Create a reference to the 3D model
+  
   const modelRef = useRef();
 
-  // Load the GLTF model
+  
   const gltf = useLoader(GLTFLoader, gltfUrl);
  
 
   return (
     <Canvas
       style={{ height: '100vh', width: '70vw' }}>
-      {/* Camera with OrbitControls */}
+      
       <OrbitControls
         enableDamping
         dampingFactor={0.5}
-        target={[100, 70, 50]} // Set the target point to look at
+        target={[100, 70, 50]} 
       />
       <PerspectiveCamera
         makeDefault
@@ -30,11 +30,11 @@ function ModelViewer() {
         fov={30}
       />
 
-      {/* Lighting */}
+      
       <ambientLight intensity={0.5} />
       <pointLight position={[10, 10, 10]} intensity={1} />
 
-      {/* Add the loaded model */}
+      
       <group ref={modelRef}>
         <primitive 
         object={gltf.scene} 
