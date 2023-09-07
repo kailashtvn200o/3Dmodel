5b9a48e5044ea651a65643e93402234d67909aff
kailashtvn200o <kailashtvn2000@gmail.com>
2023-09-07 11:44:35 +0530

    changed 3d model

--- src/Modelviewer.js
+++ src/Modelviewer.js
@@ -2,23 +2,13 @@ import React, { useRef } from 'react';
 import { Canvas, useLoader, useFrame,extend, useThree } from 'react-three-fiber';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader';
 import { OrbitControls,PerspectiveCamera } from '@react-three/drei';
-
-
-
 function ModelViewer() {
   const gltfUrl = '/scene.gltf'; 
-
-  
-  const modelRef = useRef();
-
-  
+  const modelRef = useRef();  
   const gltf = useLoader(GLTFLoader, gltfUrl);
- 
-
   return (
     <Canvas
-      style={{ height: '100vh', width: '70vw' }}>
-      
+      style={{ height: '100vh', width: '70vw' }}>    
       <OrbitControls
         enableDamping
         dampingFactor={0.5}
@@ -28,23 +18,17 @@ function ModelViewer() {
         makeDefault
         position={[10,0,5]} //x-axis,y-axis,z-axis
         fov={30}
-      />
-
-      
-      <ambientLight intensity={0.5} />
-      <pointLight position={[10, 10, 10]} intensity={1} />
-
-      
+      />     
+      <ambientLight intensity={3} />
+      <pointLight position={[2, 2, 2]} intensity={90} />     
       <group ref={modelRef}>
         <primitive 
         object={gltf.scene} 
         ref={modelRef}
-        position={[10,50,50]}
+        position={[0,0,0]}
         />
-
       </group>
     </Canvas>
   );
 }
-
 export default ModelViewer;
